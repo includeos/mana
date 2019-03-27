618512a8d66f6a454c714d0e92aff929c8792427
Martin Nordsletten <mnordsletten@gmail.com>
2019-03-27 13:11:21 +0100

    Conan: Now using python_requires for git function + latest/stable

--- conanfile.py
+++ conanfile.py
@@ -1,28 +1,11 @@
-from conans import ConanFile,tools,CMake
+from conans import ConanFile, python_requires, CMake
 
-def get_version():
-    git = tools.Git()
-    try:
-        prev_tag = git.run("describe --tags --abbrev=0")
-        commits_behind = int(git.run("rev-list --count %s..HEAD" % (prev_tag)))
-        # Commented out checksum due to a potential bug when downloading from bintray
-        #checksum = git.run("rev-parse --short HEAD")
-        if prev_tag.startswith("v"):
-            prev_tag = prev_tag[1:]
-        if commits_behind > 0:
-            prev_tag_split = prev_tag.split(".")
-            prev_tag_split[-1] = str(int(prev_tag_split[-1]) + 1)
-            output = "%s-%d" % (".".join(prev_tag_split), commits_behind)
-        else:
-            output = "%s" % (prev_tag)
-        return output
-    except:
-        return '0.0.0'
+conan_tools = python_requires("conan-tools/[>=1.0.0]@includeos/stable")
 
 class ManaConan(ConanFile):
     settings= "os","arch","build_type","compiler"
     name = "mana"
-    version = get_version()
+    version = conan_tools.git_get_semver()
     license = 'Apache-2.0'
     description = 'Run your application with zero overhead'
     generators = 'cmake'
@@ -36,7 +19,7 @@ class ManaConan(ConanFile):
 
     no_copy_source=True
     default_user="includeos"
-    default_channel="test"
+    default_channel="latest"
 
     def requirements(self):
         self.requires("includeos/[>=0.14.0,include_prerelease=True]@{}/{}".format(self.user,self.channel))
